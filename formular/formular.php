d2edf64ed4bcdbaca07119fc6d67e8fb9918a07f
Vojtěch Martin <vojtech@spstrutnov.cz>
2020-09-17 09:15:50 +0200

    Příprava na databázi

--- formular/formular.php
+++ formular/formular.php
@@ -5,6 +5,25 @@ require_once "../config.php";
 $spojeni = mysqli_connect(dbhost,dbuser, dbpass, dbname);
 session_start();
 
+$datum = date(); // datum
+$hodina = ""; // předmět
+$ucitel = ""; // učitel
 $like = $_POST["option"]; // like & dislike
 $hodnoceni = $_POST["hodnoceni"]; // text hodnocení
+
+// Názvy řádků v tabulce do té doby, než se dohodneme na názvu v databázi
+$nazevTabulky = ``;
+
+$datumTabulka = ``;
+$hodinaTabulka = ``;
+$ucitelTabulka = ``;
+$likeTabulka = ``;
+$hodnoceniTabulka = ``;
+
+//vložení dat do tabulky
+mysqli_query($spojeni, "INSERT INTO $nazevTabulky($datumTabulka,$hodinaTabulka,$ucitelTabulka,$likeTabulka,$hodnoceniTabulka) VALUES ($datum,$hodina,$ucitel,$like,$hodnoceni,)");
+
+
+header();
+
 ?>
