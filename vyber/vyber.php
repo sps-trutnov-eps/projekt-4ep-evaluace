3fa7103cb149104357e3846f40e171ceaca6000a
Tomáš Prouza <prouzat@spstrutnov.cz>
2020-09-24 14:59:45 +0200

    Zaklad_t3_student

---
+++ vyber/vyber.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vyber</title>
+</head>
+<body>
+    <header>
+        <h1>Výběr hodiny</h1>
+    </header>
+    <main>
+        <nav>
+            <form method="post" action="ZATIM_NEEXISTUJE">
+                <input type="text" name="obor" placeholder="Obor ..." /><br />
+                <input type="text" name="rocnik" placeholder="Ročník ..." /><br />
+                <input type="text" name="predmet" placeholder="Předmět ..." /><br />
+                <input type="submit" value="Potvrdit" />
+            </form>
+        </nav>
+    </main>
+    <footer>
+    </footer>
+</body>
+</html>
