15e2d114efb5d7e0dc82aedfbb4a8ebcc004feb2
Jan Zlesák <jan.zlesak.ml@seznam.cz>
2020-10-11 17:35:30 +0200

    Tlačítko na přidání otázek a jejich číslování

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Projekt třídy 4.EP, systém pro studentskou evaluaci výuky
+Team T6 - Posker, Zlesák
+Tvorba dotazníku k vytvořeným hodinám
--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let formularAnoNe = "";
+let formularText = "";
+let formularVyber = "";
+let formularHvezdy = "";
+let formularLikeDislike = "";
+let vyberFormular = "<div id='#'><h3>Otázka '#':</h3><textarea name='zadanaOtazka' id='otazka' cols='30' rows='4'></textarea><h3>Možnost odpovědi:</h3><input type='radio' name='volbaOdpovedi' value='text' id='text'><label for='text'>Text</label><br><input type='radio' name='volbaOdpovedi' value='anoNe' id='anoNe'><label for='anoNe'>Ano/Ne</label><br><input type='radio' name='volbaOdpovedi' value='vyber' id='vyber'><label for='vyber'>Výběr</label><br></div>";
+let i = 1;
+
 function pridatDalsiOtazku(){
-    $pripojitKPoslednimu = document.getElementById("otazky");
+    vyberFormularZmena = vyberFormular.replaceAll("#",i.toString());
+    let j = i-1;
+    if(j>0)
+        document.getElementById(j).insertAdjacentHTML("afterEnd",vyberFormularZmena);
+    else
+        document.getElementById("vyberCelkovehoHodnoceniHodiny").insertAdjacentHTML("afterEnd",vyberFormularZmena);
+    i++;
 }
