4d1f55e9daae789ab6ef06f3ce5b93be5e5a5cf0
Jan Zlesák <jan.zlesak.ml@seznam.cz>
2020-11-22 17:32:58 +0100

    Debug, tlačítko pro odstranění, tlačítko pro přidání výběru-vývoj

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
-let formularAnoNe = "";
+let textareaVyber = "<textarea name='podvyber' id='podvyber#*' cols='30' rows='2'></textarea>";
 let formularText = "";
-let formularVyber = "";
+let formularVyber = "<div id='pridatVyber#'><button onclick='pridatDalsiVyber(#)'>Přídat další možnost k výběru</button><br><div>";
 let formularHvezdy = "";
 let formularLikeDislike = "";
-let vyberFormular = "<div id='#'><h3>Otázka '#':</h3><textarea name='zadanaOtazka' id='otazka' cols='30' rows='4'></textarea><h3>Možnost odpovědi:</h3><input type='radio' name='volbaOdpovedi' value='text' id='text'><label for='text'>Text</label><br><input type='radio' name='volbaOdpovedi' value='anoNe' id='anoNe'><label for='anoNe'>Ano/Ne</label><br><input type='radio' name='volbaOdpovedi' value='vyber' id='vyber'><label for='vyber'>Výběr</label><br></div>";
+let vyberFormular = "<div id='#'><h3>Otázka '#':</h3><textarea name='zadanaOtazka' id='otazka' cols='30' rows='4'></textarea><h3>Možnost odpovědi:</h3><input type='radio' name='volbaOdpovedi#' value='text' id='text#' onchange='moznostOdpovedi(#)'><label for='text'>Text</label><br><input type='radio' name='volbaOdpovedi#' value='anoNe' id='anoNe#' onchange='moznostOdpovedi(#)'><label for='anoNe'>Ano/Ne</label><br><input type='radio' name='volbaOdpovedi#' value='vyber' id='vyber#' onchange='moznostOdpovedi(#)'><label for='vyber'>Výběr</label><br><button id='odstranit#' onclick='odstranitOtazku(#)'>Odstranit tuto otázku</button><br></div>";
 let i = 1;
+let pocetOtazek = 0;
+let z = 1;
+let k = 1;
 
-function pridatDalsiOtazku(){
-    vyberFormularZmena = vyberFormular.replaceAll("#",i.toString());
-    let j = i-1;
-    if(j>0)
-        document.getElementById(j).insertAdjacentHTML("afterEnd",vyberFormularZmena);
+function pridatDalsiOtazku() {
+    vyberFormularZmena = vyberFormular.replaceAll("#", i.toString());
+    if (pocetOtazek > 0)
+        document.getElementById(i-1).insertAdjacentHTML("afterEnd", vyberFormularZmena);
     else
-        document.getElementById("vyberCelkovehoHodnoceniHodiny").insertAdjacentHTML("afterEnd",vyberFormularZmena);
+        document.getElementById("vyberCelkovehoHodnoceniHodiny").insertAdjacentHTML("afterEnd", vyberFormularZmena);
+    pocetOtazek++;
     i++;
+}
+function moznostOdpovedi(cislo) {  
+    var prvky = document.getElementsByName('volbaOdpovedi' + cislo); 
+        
+    for(j = 0; j < prvky.length; j++) { 
+        if(prvky[j].checked && prvky[j].value == "vyber"){
+            formularVyberZmena = formularVyber.replaceAll("#", cislo.toString());
+            document.getElementById(cislo).insertAdjacentHTML("beforeend", formularVyberZmena);
+        }
+        else if(document.getElementById("pridatVyber" + cislo) != null && prvky[j].value != "vyber"){
+            document.getElementById("pridatVyber" + cislo).remove();
+        } 
+    } 
+}
+function odstranitOtazku(cislo){
+    document.getElementById(cislo).remove();
+    pocetOtazek--;
+}
+function pridatDalsiVyber(){
+
 }
