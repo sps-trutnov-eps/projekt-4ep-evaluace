c8dc7610dc40e521ce78d451fea2fa03785deaa8
Jiří obst <obstj@spstrutnov.cz>
2020-11-25 11:50:14 +0100

    Spojení s databázi
    vkládání do databáze

--- vyber/vyber_zpracovani.php
+++ vyber/vyber_zpracovani.php
@@ -4,6 +4,8 @@ $trida = $_POST["trida"];
 $predmet = $_POST["predmet"];
 
 $spojeni = mysqli_connect(dbhost, dbuser, dbpass, dbname);
+$data = mysqli_query($spojeni, "SELECT * FROM eval_predmety WHERE nazev = '$nazev'");
+$data = mysqli_query($spojeni, "SELECT * FROM eval_tridy WHERE trida = '$trida'");
 
 if($trida == 0)
 {
