90f79ddc74adda0003038c37f780082983df4a76
Strnad Matěj <strnad@spstrutnov.cz>
2020-12-03 15:01:19 +0100

    dodělán rozvrh, rozdělané jquery pro upravování rozvrhu

--- src/ucitel_rozvrh.js
+++ src/ucitel_rozvrh.js
@@ -1,15 +1,21 @@
 var dny = ["Po", "Út", "St", "Čt", "Pá"];
 
-function generovatDny() {
+function generovatRozvrh() {
     var pole = "";
     for (i = 0; i <= 4; i++) {
-        pole += "<tr id='" + dny[i] + "'><th>" + dny[i] + "</th></tr>";
+        pole += "<tr id='" + dny[i] + "'><th>" + dny[i] + "</th>";
+        for (y = 1; y <= 9; y++) {
+            pole += "<th id='"+ y + "_" + dny[i] + "' onclick='pridatHodinu(this.id)'></th>";
+        }
+        pole += "</tr>";
     }
     document.getElementById("rozvrh").innerHTML = pole;
 }
 
-function generovatRozvrh() {
-
+function pridatHodinu(id) {
+    $("#upravy").addEventListener("click", function () {
+        document.getElementById(id).innerHTML = ":)"; //dodělat jquery
+    })
 }
 
 function generovatTemata() {
