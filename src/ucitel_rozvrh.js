1574c7b20cb7a02ec18d977ddeee867de1611fcf
Matěj Strnad <matej.strnad88@gmail.com>
2020-12-03 19:43:09 +0100

    funkční upravování rozvrhu + vypínání úprav rozvrhu

--- src/ucitel_rozvrh.js
+++ src/ucitel_rozvrh.js
@@ -5,7 +5,7 @@ function generovatRozvrh() {
     for (i = 0; i <= 4; i++) {
         pole += "<tr id='" + dny[i] + "'><th>" + dny[i] + "</th>";
         for (y = 1; y <= 9; y++) {
-            pole += "<th id='"+ y + "_" + dny[i] + "' onclick='pridatHodinu(this.id)'></th>";
+            pole += "<th id='"+ y + "_" + dny[i] + "' onclick=''></th>";
         }
         pole += "</tr>";
     }
@@ -13,10 +13,21 @@ function generovatRozvrh() {
 }
 
 function pridatHodinu(id) {
-    $("#upravy").addEventListener("click", function () {
-        document.getElementById(id).innerHTML = ":)"; //dodělat jquery
-    })
+    document.getElementById(id).innerHTML = ":)";
 }
+
+$(document).ready(function () {
+    $("#upravy").click(function () {
+        if (document.getElementById("upravy").innerHTML == "Upravit") {
+            $("#upravy").text("Zastavit úpravy");
+            $("th").attr("onclick", "pridatHodinu(this.id)");
+
+        } else {
+            $("#upravy").text("Upravit");   
+            $("th").attr("onclick", "");
+        }
+    })
+})
 
 function generovatTemata() {
     
