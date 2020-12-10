4bb4b0d26650a074069696c1a6db9e1c98f16a28
Tomáš Sobotka <sobotka.visualstudio@email.cz>
2020-12-10 09:28:55 +0100

    Vypisování textu z JSON (v podstatě okopírováno od učitele)

--- formular/formular.php
+++ formular/formular.php
@@ -1,12 +1,57 @@
-<?php
-
-require_once "../config.php";
-
-$spojeni = mysqli_connect(dbhost,dbuser, dbpass, dbname);
-session_start();
-
-$like = $_POST["option"]; // like & dislike
-$hodnoceni = $_POST["hodnoceni"]; // text hodnocení
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Za to může Honza</title>
+</head>
+<body>
+    <?php
+    require_once "../config.php";
+    $spojeni = mysqli_connect(dbhost, dbuser,dbpass,dbname);
+    session_start();
+    //$_POST["něco"];
+    //$formular = mysqli_query($spojeni, "SELECT * FROM databaze WHERE neco = 'neco'");
+    //json_decode($formular);
+    $data = '[
+        {
+            "typ": "otevrena",
+            "text": "Otevřená otázka"
+        },
+        {
+            "typ": "uzavrena",
+            "text": "Uzavřená otázka",
+            "moznosti": [
+                { "text": "První možnost" },
+                { "text": "Druhá možnost" },
+                { "text": "Třetí možnost" }
+            ]
+        }
+    ]';
+    
+    $objekt = json_decode($data);    
+    foreach($objekt as $otazka){
+        switch($otazka->typ){
+            case "otevrena":
+                $nadpis = $otazka->text;
+                echo $nadpis;
+                break;
+            case "hvezdicky":
+                $nadpis = $otazka->text;
+                echo $nadpis;
+                break;
+            case "moznosti":
+                $nadpis = $otazka->text;
+                echo $nadpis;
+                break;
+            case "bezHodnoceni";
+                $nadpis = $otazka->text;
+                echo $nadpis;
+                break;
+            default:
+                /*Honzo nefunguje ti to!*/
+        }
+    }
+    ?>
+</body>
+</html>
