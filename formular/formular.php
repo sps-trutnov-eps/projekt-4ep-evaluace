186bd1e2402e4e7a719df79240ff6e02f969a372
Martin Vojtěch <vojtech@spstrutnov.cz>
2020-12-10 09:35:24 +0100

    kod

--- formular/formular.php
+++ formular/formular.php
@@ -22,17 +22,92 @@
      { 
         echo "Připojení do databáze selhalo."; 
      } 
+     else
+     {
+        //ZBYTEK KÓDU SEM
+     }
 
      $formularIdRozvrh = $_POST[]; // rozvrh pošle info jaký formular se má vypsat
-     $formular = mysqli_query($table, "SELECT * FROM WHERE "); // vybere všechny informace k formuláři který se má vypsat
+
+     $formular = mysqli_query($table, "SELECT FROM WHERE = $formularIdRozvrh"); // vybere všechny informace k formuláři který se má vypsat
      $row = mysqli_num_rows($formular); // počet nalezených formuláří s id
 
      if($row == 1)
      {
-        echo "<table><th>";
 
-        echo "</th>";
-        <tr><td>table data</td></tr></table>
+        // ZDE BUDE ROZEBRÁN STRING OD ZLESÁKA A UTVOŘEN Z TOHO FORMULÁŘ
+
+        $formularDotazu = json_decode($formular,true);
+        $nazevDotazniku = "Zde nám zdělte váš názor";
+
+        //prvni Ano/Ne, hvezdy, Bez Hodnoceni
+
+       //jedna velká forma a skládat ji?(zde začátek form?)
+       echo "<form action='' method='post' id ='formularFORM'>";
+
+       foreach($formularDotazu as $otazka)
+       {
+
+           switch ($otazka->typ) {
+            
+            case 'AnoNe':
+                echo "
+                <table><tr>
+                <th>$nazevDotazniku</th>
+                <td><input type='radio' id='like' value='like' name='option'><label for=''>Like</label></td>
+                <td><input type='radio' id='dislike' value='dislike' name='option'><label for=''>Dislike</label></td>
+                </tr></table>
+                "
+                ;
+                break;
+
+            case 'Hvezda':
+                echo "
+                <table><tr>
+                <th>$nazevDotazniku</th>
+                <td><input type='radio' id='1' value='1' name='hvezda'></td>
+                <td><input type='radio' id='2' value='2' name='hvezda'></td>
+                <td><input type='radio' id='3' value='3' name='hvezda'></td>
+                <td><input type='radio' id='4' value='4' name='hvezda'></td>
+                <td><input type='radio' id='5' value='5' name='hvezda'></td>
+                </tr></table>
+                "
+                ;
+                break;
+
+            case 'BezHodnoceni':
+                echo "
+                <table><tr>
+                <th>$nazevDotazniku<th>
+                </tr><table>
+                "
+                ;
+                break;
+
+            case 'text':
+                echo "
+                <table><tr>
+                <th>'.$otazka->text.'<th>
+                </tr><table>
+                "
+                ;
+                break;
+                
+
+                    
+
+            default:
+                echo "                
+                <table><tr>
+                <th>Někde se stala chyba<th>
+                </tr><table>";
+                break;
+        }
+
+       }
+
+        echo "</form>"
+
      }
      else
      {
@@ -64,12 +139,17 @@
                 <tr>
                     <td colspan="2"><textarea placeholder="Zde napište vaše případné poznámky." name="text" id="text" style="resize: none;" maxlength="250" cols="40" rows="10"></textarea></td>
                 </tr>
+                
                 <tr>
                     <td><input type="submit" value="Odeslat"><td>
                 </tr>
             </table>
         </form>
     </body>
+
+
+
+
 
     <footer>
         <i>&copy; Günther, Provazník, Sobotka, Vojtěch, Vaněk 2020</i>
