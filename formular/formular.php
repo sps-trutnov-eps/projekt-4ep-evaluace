4b1bcd463f3725156b93920b7b11ec83f1501ca4
SobotkaT <sobotka.visualstudio@email.cz>
2020-12-12 15:19:28 +0100

    Zobrazení otevřených otázek a otázek s výběrem odpovědi

--- formular/formular.php
+++ formular/formular.php
@@ -10,8 +10,8 @@
     require_once "../config.php";
     $spojeni = mysqli_connect(dbhost, dbuser,dbpass,dbname);
     session_start();
-    //$_POST["něco"];
-    //$formular = mysqli_query($spojeni, "SELECT * FROM databaze WHERE neco = 'neco'");
+    //$_POST["něco"]; //-> tady přijde id formuláře
+    //$formular = mysqli_query($spojeni, "SELECT * FROM databaze WHERE neco = 'neco'"); //-> tady se vytáhne formulář a uloží se
     //json_decode($formular);
     $data = '[
         {
@@ -19,8 +19,9 @@
             "text": "Otevřená otázka"
         },
         {
-            "typ": "uzavrena",
-            "text": "Uzavřená otázka",
+            "typ": "moznosti",
+            "text": "Uzavřená otázka s jednou možností",
+            "pocetOdp": "1",
             "moznosti": [
                 { "text": "První možnost" },
                 { "text": "Druhá možnost" },
@@ -30,27 +31,32 @@
     ]';
     
     $objekt = json_decode($data);    
+    $cisloOtazky = 0; //číslování otázek
     foreach($objekt as $otazka){
+        $cisloOtazky ++;
+        echo "<table>";
         switch($otazka->typ){
             case "otevrena":
                 $nadpis = $otazka->text;
-                echo $nadpis;
-                break;
-            case "hvezdicky":
-                $nadpis = $otazka->text;
-                echo $nadpis;
+                echo "<th id='$cisloOtazky'>$nadpis</th>";
+                echo "<td><textarea name='odpoved' id='$cisloOtazky' cols='30' rows='5' placeholder='Tvá odpověď přijde sem...'></textarea></td>";
                 break;
             case "moznosti":
                 $nadpis = $otazka->text;
-                echo $nadpis;
+                echo "<th id='$cisloOtazky'>$nadpis</th>";
+                $cisloOdpovedi = 1; //číslování možností, kvůli zjištění o jakou odpověď se jednalo
+                foreach($otazka->moznosti as $moznost){
+                    echo "<td><input type='radio' id='$cisloOdpovedi' name='$cisloOtazky' value='$moznost->text'><label for='$moznost->text'>$moznost->text</label></td>";
+                    $cisloOdpovedi ++;
+                }
                 break;
-            case "bezHodnoceni";
+            case "hvezdicky":
                 $nadpis = $otazka->text;
-                echo $nadpis;
                 break;
             default:
                 /*Honzo nefunguje ti to!*/
         }
+        echo "</table>";
     }
     ?>
 </body>
