17d98b7c5037329ff27661605c85c54bf92dba6c
posker <posker@spstrutnov.cz>
2020-12-17 14:44:13 +0100

    Uložení formuláře pouze s vybranou hodinou

--- ajax.php
+++ ajax.php
@@ -6,21 +6,22 @@
     if (isset($_POST["a"])) {
         $action = $_POST['a'];
         if ($action == "otazky") {
-            $array = $_POST['array'];
-
-            if(isset($_POST['nazev'])) {
-                $nazev = $_POST['nazev'];
-            } else {
-                $nazev = "";
-            }
-        
-            $ucitelID = $_SESSION['idUcitel'];
-
-            mysqli_query($spojeni, "INSERT INTO eval_formulare_vzory (`otazka`,`idUcitel`,`nazev`) VALUES ('$array','$ucitelID','$nazev')");
-            
-            $idForm = mysqli_insert_id($spojeni);
             $idHodiny = $_POST['idHodiny'];
-
-            mysqli_query($spojeni, "INSERT INTO eval_formulare (`idVzoru`, `idHodiny`) VALUES ('$idForm','$idHodiny')");
+            if($idHodiny == "") {
+                echo "NeniVybranaHodina";
+            } else {
+                $array = $_POST['array'];
+                $ucitelID = $_SESSION['idUcitel'];
+                if(isset($_POST['nazev'])) {
+                    $nazev = $_POST['nazev'];
+                    mysqli_query($spojeni, "INSERT INTO eval_formulare_vzory (`otazka`,`idUcitel`,`nazev`) VALUES ('$array','$ucitelID','$nazev')");
+                } else {
+                    mysqli_query($spojeni, "INSERT INTO eval_formulare_vzory (`otazka`,`idUcitel`) VALUES ('$array','$ucitelID')");
+                }
+                                
+                $idForm = mysqli_insert_id($spojeni);
+    
+                mysqli_query($spojeni, "INSERT INTO eval_formulare (`idVzoru`, `idHodiny`) VALUES ('$idForm','$idHodiny')");
+            }
         }
     }
