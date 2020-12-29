90459fad120356a54d268e383498fb950508f6f0
Jan Zlesák <jan.zlesak.ml@seznam.cz>
2020-12-29 21:01:34 +0100

    Kontrola $Session kvůli ID

--- ucitel_prihlaseni.php
+++ ucitel_prihlaseni.php
@@ -4,30 +4,26 @@ require_once "../../config.php";
 
 $email = $_POST["email"];
 $passwd = $_POST["passwd"];
-
 $spojeni = mysqli_connect(dbhost, dbuser, dbpass, dbname);
-$data = mysqli_query($spojeni, "SELECT * FROM eval_ucitele WHERE email = '$email'");
 
-if(mysqli_num_rows($data) == 0)
-{
-    echo "<p>Tento email: <b>$email</b> neexistuje.</p>";
-}
-else
-{
-    $email = mysqli_fetch_assoc($data);
-    if(password_verify($passwd, $email["passwd"]))
-    {
-        session_start();
-        $_SESSION['idUcitel'] = $email["id"];
-        header("location:ucitel_rozvrh.html");
-    }
-    else if ($passwd == NULL) {
-        echo "<p>Vyplňte heslo.</p>";
-    }
-    else if(!password_verify($passwd, $email["passwd"]))
-    {
-        echo "<p>Neplatné heslo pro email</p>";
-    }
-}
+session_start();
+if ($_SESSION['idUcitel'] == NULL) {
+    $data = mysqli_query($spojeni, "SELECT * FROM eval_ucitele WHERE email = '$email'");
 
+    if (mysqli_num_rows($data) == 0) {
+        echo "<p>Tento email: <b>$email</b> neexistuje.</p>";
+    } else {
+        $email = mysqli_fetch_assoc($data);
+        if (password_verify($passwd, $email["passwd"])) {
+            $_SESSION['idUcitel'] = $email["id"];
+            header("location:ucitel_rozvrh.html");
+        } else if ($passwd == NULL) {
+            echo "<p>Vyplňte heslo.</p>";
+        } else if (!password_verify($passwd, $email["passwd"])) {
+            echo "<p>Neplatné heslo pro email</p>";
+        }
+    }
+} else {
+    header("location:ucitel_rozvrh.html");
+}
 mysqli_close($spojeni);
