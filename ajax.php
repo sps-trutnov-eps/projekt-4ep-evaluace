08816da28f99f56c3461775a56e99ceb8409e0a4
Jan Zlesák <jan.zlesak.ml@seznam.cz>
2020-12-30 11:06:19 +0100

    Sjednocení adresace

--- ajax.php
+++ ajax.php
@@ -1,10 +1,7 @@
 <?php  
     session_start();
-    //odstranit po debugu !!!!!
-    $_SESSION["idUcitel"] = 2;//odstranit po debugu !!!!!
-        require_once "../../config.php";
-        $spojeni = mysqli_connect(dbhost, dbuser, dbpass, dbname);
-    //odstranit po debugu !!!!!
+    require_once "../../config.php";
+    $spojeni = mysqli_connect(dbhost, dbuser, dbpass, dbname);
     if (isset($_POST["a"])) {
         $action = $_POST['a'];
         if ($action == "otazky") {
