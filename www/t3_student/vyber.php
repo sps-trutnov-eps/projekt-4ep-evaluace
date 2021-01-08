3961fcc14ec2b2a8f1b9b39c7b156f6806ffbbfe
Ilchmann <ilchmann@spstrutnov.cz>
2021-01-08 14:44:06 +0100

    Přidáno zobrazení css na strance žáka

--- www/t3_student/vyber.php
+++ www/t3_student/vyber.php
@@ -2,7 +2,7 @@
 <html lang="cs">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="../css/student.css">
+    <link rel="stylesheet" href="../css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Výběr</title>
 
